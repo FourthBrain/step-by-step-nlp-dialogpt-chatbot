63cfc9b0ef79db7fa8e6547f5de7fa95bf86a693
FourthBrain <72572922+FourthBrain@users.noreply.github.com>
2021-11-16 18:29:19 -0800

    Add files via upload

---
+++ dialogpt-chatbot/README.md
@@ -0,0 +1,20 @@
+# Gotham-chatbot
+
+
+## Flask Chatbot
+
+### Here's how it looks:
+![Screenshot (189)](https://user-images.githubusercontent.com/72303641/139554258-33d11254-8a1b-4ec9-b780-7061dc1cfe37.png)
+
+
+#### A web implementation of [ChatterBot](https://github.com/gunthercox/ChatterBot) using Flask.
+
+## Local Setup:
+ 1. Ensure that Python, Flask, SQLAlchemy, and ChatterBot are installed (either manually, or run `pip install -r requirements.txt`).
+ 2. Run *app.py* with `python app.py`.
+ 3. The demo will be live at [http://localhost:5000/](http://localhost:5000/)
+
+
+## License
+This source is free to use, but ChatterBot does have a license which still applies and can be found on the 
+[LICENSE](https://github.com/gunthercox/ChatterBot/blob/master/LICENSE) page.
---
+++ dialogpt-chatbot/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+app = Flask(__name__)
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(userText + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        
+        return str(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
+
+
+if __name__ == "__main__":
+    app.run()
+
+
